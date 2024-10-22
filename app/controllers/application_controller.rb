9c9a0972861797af64e96212ee154e1f2cdbc8d5
Sumit Maurya <mauryasumit222@gmail.com>
2024-10-22 18:42:39 +0530

    Complete_login_funtionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path
   end
-
-  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :surname, :date_of_birth, :gender ])
